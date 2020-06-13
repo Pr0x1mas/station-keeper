4975201a8f79900eed1fc0c9c58630275b68f557
HyperHamster535 <hyperhamster534@outlook.com>
2020-06-13 12:21:35 +0100

    added typing indicator when bot is finding station

--- main.py
+++ main.py
@@ -20,30 +20,29 @@ async def on_ready():
 
     
 @bot.command()
-async def findCarrier(ctx, station):
-    try:
-        carriername = station.replace(" ", "%20") # convert spaces to %20 for url
+async def findCarrier(ctx, station): # finds a station or fleet carrier
+    async with ctx.typing():
+        try:
+            carriername = station.replace(" ", "%20") # convert spaces to %20 for url
 
-        url = 'https://eddbapi.kodeblox.com/api/v4/stations?name=' + carriername # get data for station
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        raw_station = urlopen(req).read()
+            url = 'https://eddbapi.kodeblox.com/api/v4/stations?name=' + carriername # get data for station
+            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            raw_station = urlopen(req).read()
 
-        station = json.loads(raw_station) # parse station data as json and get system id
-        systemid = station.get("docs")[0].get("system_id")
+            station = json.loads(raw_station) # parse station data as json and get system id
+            systemid = station.get("docs")[0].get("system_id")
 
-        url = 'https://eddbapi.kodeblox.com/api/v4/systems?eddbid=' + str(systemid) # get data for system
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        raw_system = urlopen(req).read()
+            url = 'https://eddbapi.kodeblox.com/api/v4/systems?eddbid=' + str(systemid) # get data for system
+            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            raw_system = urlopen(req).read()
 
-        system = json.loads(raw_system) # parse system data as json and get system name
-        systemname = system.get("docs")[0].get("name")
+            system = json.loads(raw_system) # parse system data as json and get system name
+            systemname = system.get("docs")[0].get("name")
 
-        print("Carrier/station " + carriername + " is at system " + systemname)
+            await ctx.send("Carrier/station " + carriername + " is at system " + systemname) # send message showing whereabouts of station
 
-        await ctx.send("Carrier/station " + carriername + " is at system " + systemname)
-    
-    except Exception:
-        await ctx.send("Carrier/station " + carriername + " could not be found")
+        except Exception:
+            await ctx.send("Carrier/station " + carriername + " could not be found") # if there are any errors, show this message
 
 if __name__ == "__main__":
     bot.run(TOKEN)
